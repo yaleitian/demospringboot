55553ac3cba3bd9eb7c435c3e01e7e1eb0d61f08
tianyalei <tianyaleimessage@gmail.com>
2019-11-19 14:37:32 +0800

    test read

--- src/test/java/com/example/demospringboot/TestWordData.java
+++ src/test/java/com/example/demospringboot/TestWordData.java
@@ -1,10 +1,12 @@
 package com.example.demospringboot;
 
-import com.example.demospringboot.readword.ReadDocx;
-import com.example.demospringboot.readword.ThreadWord;
-import com.example.demospringboot.readword.WordData;
+import com.example.demospringboot.readword.*;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -56,6 +58,98 @@ public class TestWordData {
         new Thread( threadWord ).start();
         new Thread( threadWord ).start();
         new Thread( threadWord ).start();
+        long nano_endTime = System.nanoTime();
+        long millis_endTime = System.currentTimeMillis();
+        System.out.println("Time taken in nano seconds: "
+                + (nano_endTime - nano_startTime));
+        System.out.println("Time taken in milli seconds: "
+                + (millis_endTime - millis_startTime));
+    }
+
+    @Test
+    public void convertTxt(){
+        long nano_startTime = System.nanoTime();
+        long millis_startTime = System.currentTimeMillis();
+        String filePath = "/Users/tianyalei/Downloads/word";
+        String findName = "/Users/tianyalei/Downloads/txt";
+        WordData wordData = new WordData();
+        List<String> list = wordData.convertTxt( filePath, findName );
+        System.out.println(list);
+        long nano_endTime = System.nanoTime();
+        long millis_endTime = System.currentTimeMillis();
+        System.out.println("Time taken in nano seconds: "
+                + (nano_endTime - nano_startTime));
+        System.out.println("Time taken in milli seconds: "
+                + (millis_endTime - millis_startTime));
+    }
+
+    @Test
+    public void parseText() throws IOException {
+        long nano_startTime = System.nanoTime();
+        long millis_startTime = System.currentTimeMillis();
+        String txt = "";
+        String fileName = "";
+        String suffix = "";
+        byte[] bytes = null;
+        String filePath = "/Users/tianyalei/Downloads/word";
+        String findName = "任务";
+        ParseText parseText = new ParseText();
+        List<String> list = new ArrayList<>();
+        File file = new File( filePath );
+        File[] arrFiles = file.listFiles();
+        for (File getFile : arrFiles) {
+            fileName = getFile.getName();
+            bytes = Files.readAllBytes(new File(getFile.getPath()).toPath());
+            suffix = fileName.substring(fileName.lastIndexOf(".") + 1);
+            txt = parseText.parse( bytes, suffix );
+            //判断出现过搜索的字符串
+            int num = txt.indexOf( findName );
+            if (num > 0) {
+                list.add( fileName );
+            }
+        }
+        System.out.println(list);
+        System.out.println(list.size());
+        long nano_endTime = System.nanoTime();
+        long millis_endTime = System.currentTimeMillis();
+        System.out.println("Time taken in nano seconds: "
+                + (nano_endTime - nano_startTime));
+        System.out.println("Time taken in milli seconds: "
+                + (millis_endTime - millis_startTime));
+    }
+
+    @Test
+    public void readDoc() throws IOException {
+        long nano_startTime = System.nanoTime();
+        long millis_startTime = System.currentTimeMillis();
+        String txt = "";
+        String fileName = "";
+        //String suffix = "";
+        //byte[] bytes = null;
+        String filePath = "/Users/tianyalei/Downloads/word";
+        String findName = "五月";
+        //ParseText parseText = new ParseText();
+        ReadDoc readDoc = new ReadDoc();
+        List<String> list = new ArrayList<>();
+        File file = new File( filePath );
+        File[] arrFiles = file.listFiles();
+        for (File getFile : arrFiles) {
+            fileName = getFile.getName();
+            filePath = filePath + "/" + fileName;
+            //bytes = Files.readAllBytes(new File(getFile.getPath()).toPath());
+            //suffix = fileName.substring(fileName.lastIndexOf(".") + 1);
+            //txt = parseText.parse( bytes, suffix );
+            FileInputStream in;
+            in = new FileInputStream( getFile );
+            txt = readDoc.readDoc( filePath, in );
+            //判断出现过搜索的字符串
+            int num = txt.indexOf( findName );
+            if (num > 0) {
+                list.add( fileName );
+            }
+        }
+        System.out.println(list);
+        System.out.println(list.size());
         long nano_endTime = System.nanoTime();
         long millis_endTime = System.currentTimeMillis();
         System.out.println("Time taken in nano seconds: "

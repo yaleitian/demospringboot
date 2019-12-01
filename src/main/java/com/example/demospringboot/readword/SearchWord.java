d91c552b7d2a4906cf2a67d7492c5610921e61a6
tianyalei <tianyaleimessage@gmail.com>
2019-12-02 05:09:02 +0800

    add SearchWord

---
+++ src/main/java/com/example/demospringboot/readword/SearchWord.java
@@ -0,0 +1,33 @@
+package com.example.demospringboot.readword;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+
+/**
+ * @author tianyalei
+ */
+public class SearchWord implements Callable<Integer> {
+        private int localCounter = 0;
+        // start index of search
+        private int start;
+        private int end;
+        private List<String> words;
+        private String token;
+
+        public SearchWord(int start, int end, List<String> words, String token) {
+            this.start = start;
+            this.end = end;
+            this.words = words;
+            this.token = token;
+        }
+
+        @Override
+        public Integer call() {
+            for(int i = start; i < end; i++) {
+                if(words.get(i).equals(token)) {
+                    localCounter++;
+                }
+            }
+            return localCounter;
+        }
+}

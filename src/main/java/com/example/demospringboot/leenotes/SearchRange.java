652e551753bc7049700fe6bd075a3bee9255f9e6
tianyalei <tianyaleimessage@gmail.com>
2019-12-17 17:57:17 +0800

    add SearchRange.java

---
+++ src/main/java/com/example/demospringboot/leenotes/SearchRange.java
@@ -0,0 +1,29 @@
+package com.example.demospringboot.leenotes;
+
+/**
+ * @author tianyalei
+ */
+public class SearchRange {
+    public int[] searchRange(int[] nums, int target) {
+        int first = findFirst(nums, target);
+        int last = findFirst(nums, target + 1) - 1;
+        if (first == nums.length || nums[first] != target) {
+            return new int[]{-1, -1};
+        } else {
+            return new int[]{first, Math.max(first, last)};
+        }
+    }
+
+    private int findFirst(int[] nums, int target) {
+        int l = 0, h = nums.length; // 注意 h 的初始值
+        while (l < h) {
+            int m = l + (h - l) / 2;
+            if (nums[m] >= target) {
+                h = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return l;
+    }
+}

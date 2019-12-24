70682b46dc20d21c5affcdac765514c97f528793
tianyalei <tianyaleimessage@gmail.com>
2019-12-24 10:34:41 +0800

    add OrderArrary.java

---
+++ src/main/java/com/example/demospringboot/leenotes/OrderArrary.java
@@ -0,0 +1,27 @@
+package com.example.demospringboot.leenotes;
+
+/**
+ * @author tianyalei
+ */
+public class OrderArrary {
+    public void reOrderArray(int[] nums) {
+        int N = nums.length;
+        for (int i = N - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (isEven(nums[j]) && !isEven(nums[j + 1])) {
+                    swap(nums, j, j + 1);
+                }
+            }
+        }
+    }
+
+    private boolean isEven(int x) {
+        return x % 2 == 0;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+}

9c2ee4ffd7670027e1dc7770457fde5753ef995e
tianyalei <tianyaleimessage@gmail.com>
2020-01-06 06:40:17 +0800

    Java Fixed Size Thread Pool Executor

---
+++ src/main/java/com/example/demospringboot/leenotes/TaskFix.java
@@ -0,0 +1,26 @@
+package com.example.demospringboot.leenotes;
+
+import java.util.concurrent.TimeUnit;
+
+class TaskFix implements Runnable {
+    private String name;
+
+    public TaskFix(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Long duration = (long) (Math.random() * 10);
+            System.out.println( "Doing a task during : " + name );
+            TimeUnit.SECONDS.sleep( duration );
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}

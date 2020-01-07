ef13003e146a151e083a00e8760119c439142469
tianyalei <tianyaleimessage@gmail.com>
2020-01-07 08:41:34 +0800

    update ThreadPoolExample.java

--- src/main/java/com/example/demospringboot/leenotes/ThreadPoolExample.java
+++ src/main/java/com/example/demospringboot/leenotes/ThreadPoolExample.java
@@ -4,10 +4,12 @@ package com.example.demospringboot.leenotes;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
+/**
+ * @author tianyalei
+ */
 public class ThreadPoolExample {
     public static void main(String[] args) {
         ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool( 2 );
-
         for (int i = 1; i <= 5; i++) {
             TaskThreadPool task = new TaskThreadPool( "Task " + i );
             System.out.println( "Created : " + task.getName() );

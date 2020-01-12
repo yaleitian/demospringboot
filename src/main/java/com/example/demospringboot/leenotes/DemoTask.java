0536c039a38d495fa1f683b0c5a416659a31ef8a
tianyalei <tianyaleimessage@gmail.com>
2020-01-13 05:50:22 +0800

    Previous Solution Using CustomThreadPoolExecutor with RejectedExecutionHandler

---
+++ src/main/java/com/example/demospringboot/leenotes/DemoTask.java
@@ -0,0 +1,26 @@
+package com.example.demospringboot.leenotes;
+
+/**
+ * @author tianyalei
+ */
+public class DemoTask implements Runnable {
+    private String name = null;
+
+    public DemoTask(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep( 1000 );
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println( "Executing : " + name );
+    }
+}

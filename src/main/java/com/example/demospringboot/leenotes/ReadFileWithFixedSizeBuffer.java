2e55fc2c584b8c9211de35a61d99dc5b31b88d11
tianyalei <tianyaleimessage@gmail.com>
2020-01-16 11:00:36 +0800

    ReadFileWithFixedSizeBuffer.java

---
+++ src/main/java/com/example/demospringboot/leenotes/ReadFileWithFixedSizeBuffer.java
@@ -0,0 +1,24 @@
+package com.example.demospringboot.leenotes;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class ReadFileWithFixedSizeBuffer {
+    public static void main(String[] args) throws IOException {
+        RandomAccessFile aFile = new RandomAccessFile
+                ( "test.txt", "r" );
+        FileChannel inChannel = aFile.getChannel();
+        ByteBuffer buffer = ByteBuffer.allocate( 1024 );
+        while (inChannel.read( buffer ) > 0) {
+            buffer.flip();
+            for (int i = 0; i < buffer.limit(); i++) {
+                System.out.print( (char) buffer.get() );
+            }
+            buffer.clear(); // do something with the data and clear/compact it.
+        }
+        inChannel.close();
+        aFile.close();
+    }
+}

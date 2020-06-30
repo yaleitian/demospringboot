5ceecba8c4678d4f4265cef04e1bc1979e307db7
tianyalei <tianyaleimessage@gmail.com>
2020-06-30 14:44:33 +0800

    update Derived

--- src/main/java/com/example/demospringboot/initbase/Derived.java
+++ src/main/java/com/example/demospringboot/initbase/Derived.java
@@ -3,39 +3,40 @@ package com.example.demospringboot.initbase;
 class Base {
     // 1.父类静态代码块
     static {
-        System.out.println("Base static block!");
+        System.out.println( "Base static block!" );
     }
     // 3.父类非静态代码块
+
     /**
      * 4.父类构造器
-     *
      */
     public Base() {
-        System.out.println("Base constructor!");
+        System.out.println( "Base constructor!" );
     }
 
     {
-        System.out.println("Base block");
+        System.out.println( "Base block" );
     }
 }
 
 public class Derived extends Base {
     /**
      * 6.子类构造器
-     *
      */
     public Derived() {
-        System.out.println("Derived constructor!");
+        System.out.println( "Derived constructor!" );
     }
+
     // 2.子类静态代码块
-    static{
-        System.out.println("Derived static block!");
+    static {
+        System.out.println( "Derived static block!" );
     }
     // 5.子类非静态代码块
 
     {
-        System.out.println("Derived block!");
+        System.out.println( "Derived block!" );
     }
+
     public static void main(String[] args) {
         new Derived();
     }
